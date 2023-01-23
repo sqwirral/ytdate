4c54417a8eca659b34a1ee38f790492605cf5633
sqwirral <sqwirral@gmail.com>
2023-01-23 23:11:32 +0000

    Messing around

---
+++ app2.js
@@ -0,0 +1,42 @@
+// '<p class="gameId" data-id="' . html($d['id']) . '"></p>';
+
+
+let blah = document.createElement('h1');
+blah.textContent = "This page has been eaten";
+document.body.appendChild(blah);
+
+
+
+
+// const title = document.querySelector("h1.ytd-watch-metadata");
+// title.addEventListener('click', event => {
+//   alert("You clicked da ting.");
+// })
+
+// const newDiv = document.createElement("div");
+// const newContent = document.createTextNode("Yt2Clip");
+// newDiv.appendChild(newContent);
+// const currentDiv = document.getElementById("title");
+// document.body.insertBefore(newDiv, currentDiv);
+// document.body.insertAfter(newDiv, currentDiv);
+
+
+
+
+
+// document.querySelectorAll('.gameId').forEach(item => {
+//   item.addEventListener('click', event => {
+  
+//     /* Get the text field */
+//     var copyText = event.currentTarget.dataset.id;
+
+//     /* Copy the text inside the text field */
+//     navigator.clipboard.writeText(copyText);
+
+//     /* Alert the copied text */
+//     //alert("Copied the text: " + copyText);
+//     event.currentTarget.insertAdjacentHTML('afterend', 
+//       '<span style="color:#12b212;font-weight:bold">Copied!</span><br>');
+  
+//   });
+// });
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
-https://www.youtube.com/watch?v=WiLu-2GbkFg
+# Youtube to Clipboard
+
+Right-click on a tab and click "Youtube to Clipboard". Copies page title and URL to Windows clipboard. Actually works on any page, not just Youtube.
+
+With help from tutorials at
+- https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_second_WebExtension
+- https://www.youtube.com/watch?v=WiLu-2GbkFg
