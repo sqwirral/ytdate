7a4db783998b86b58a51c005600e3576fbda0552
sqwirral <sqwirral@gmail.com>
2023-01-23 21:20:29 +0000

    Trimmed junk, changed icon

--- app.js
+++ app.js
@@ -5,76 +5,21 @@ async function getSelectedTabs() {
   });
 }
 
-browser.browserAction.onClicked.addListener(copyTitleAndUrl);
-
-const copyTitleAndUrlsMenuId = "copy-selected-tab-info";
 browser.contextMenus.create({
-  id: copyTitleAndUrlsMenuId,
-  title: "Send to Twitch",
+  id: "yt2clip",
+  title: "Youtube to Clipboard",
   contexts: ["tab"],
 });
-
-const copyAsMarkdownMenuID = "copy-markdown";
-browser.contextMenus.create({
-  id: copyAsMarkdownMenuID,
-  title: "Send to Markdown",
-  contexts: ["tab"],
-});
-
-function tabsToMarkdown(tabs, returnAsList = false, separator = " ") {
-  return tabs
-    .map((tab) => `${returnAsList ? "* " : ""}[${tab.title}](${tab.url})`)
-    .join(separator);
-}
-
-async function copyMarkdown() {
-  const markdown = tabsToMarkdown(await getSelectedTabs());
-  navigator.clipboard.writeText(markdown);
-}
-
-function tabsToText(tabs, options) {
-  function setMissingOptionsToDefault(options) {
-    const defaultOptions = {
-      includeTitle: true,
-      titleUrlSeparator: " - ",
-      tabStringSeparator: "\n",
-    };
-    return Object.assign({}, defaultOptions, options);
-  }
-  options = setMissingOptionsToDefault(options);
-  return tabs
-    .map((tab) => tabToString(tab, options))
-    .join(options.tabStringSeparator);
-}
-
-function tabToString(tab, option) {
-  let result = "";
-  if (options.includeTitle) {
-    result += tab.title;
-    result += options.titleUrlSeparator;
-  }
-  result += tab.url;
-  return result;
-}
-
-async function copyTitleAndUrl() {
-  const titlesAndUrlsString = tabsToText(await getSelectedTabs());
-  navigator.clipboard.writeText(titlesAndUrlsString);
-  var params = "title=" + titlesAndUrlsString;
-}
-
-async function copyMarkdown() {
-  const markdown = tabsToMarkdown(await getSelectedTabs());
-  navigator.clipboard.writeText(markdown);
-}
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
-  switch (info.menuItemId) {
-    case copyTitleAndUrlsMenuId:
-      copyTitleAndUrl();
-      break;
-    case copyAsMarkdownMenuID:
-      copyMarkdown();
-      break;
-  }
+  copyInfo();
 });
+
+async function copyInfo() {
+  const text = tabsToText(await getSelectedTabs());
+  navigator.clipboard.writeText(text);
+}
+
+function tabsToText(tabs) {
+  return tabs[0].title + "\n" + tabs[0].url;
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,1 @@
 https://www.youtube.com/watch?v=WiLu-2GbkFg
-https://github.com/mbcrump/nodefirefoxtwitch/tree/main/firefoxv1
