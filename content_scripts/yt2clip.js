9c09047fc2fd4f6e3bfe3aa0a142404305a5f6fe
sqwirral <sqwirral@gmail.com>
2023-01-24 00:17:40 +0000

    Messing 2

--- content_scripts/yt2clip.js
+++ content_scripts/yt2clip.js
@@ -1,47 +1,19 @@
 (() => {
   /**
-   * Check and set a global guard variable.
-   * If this content script is injected into the same page again,
-   * it will do nothing next time.
+   * 
    */
-  if (window.hasRun) {
-    return;
-  }
-  window.hasRun = true;
-
-  /**
-   * Given a URL to a beast image, remove all existing beasts, then
-   * create and style an IMG node pointing to
-   * that image, then insert the node into the document.
-   */
-  function insertBeast(beastURL) {
-    removeExistingBeasts();
-    const beastImage = document.createElement("img");
-    beastImage.setAttribute("src", beastURL);
-    beastImage.style.height = "100vh";
-    beastImage.className = "beastify-image";
-    document.body.appendChild(beastImage);
-  }
-
-  /**
-   * Remove every beast from the page.
-   */
-  function removeExistingBeasts() {
-    const existingBeasts = document.querySelectorAll(".beastify-image");
-    for (const beast of existingBeasts) {
-      beast.remove();
-    }
+  function runStuff(beastURL) {
+    const createdThing = document.createElement("h1");
+    createdThing.style.height = "100vh";
+    createdThing.className = "omg wat lol";
+    createdThing.textContent = "hellloooo woooorrrrld112222!";
+    document.body.appendChild(createdThing);
   }
 
   /**
    * Listen for messages from the background script.
-   * Call "insertBeast()" or "removeExistingBeasts()".
    */
   browser.runtime.onMessage.addListener((message) => {
-    if (message.command === "beastify") {
-      insertBeast(message.beastURL);
-    } else if (message.command === "reset") {
-      removeExistingBeasts();
-    }
+    runStuff(message.blablah);
   });
 })();
