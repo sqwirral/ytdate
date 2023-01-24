35d0c1480691b4ff8bcc9dc690be414702cd47f4
sqwirral <sqwirral@gmail.com>
2023-01-24 00:22:43 +0000

    Pass text from popup

--- content_scripts/yt2clip.js
+++ content_scripts/yt2clip.js
@@ -2,11 +2,12 @@
   /**
    * 
    */
-  function runStuff(beastURL) {
+  function runStuff(text) {
     const createdThing = document.createElement("h1");
     createdThing.style.height = "100vh";
     createdThing.className = "omg wat lol";
     createdThing.textContent = "hellloooo woooorrrrld112222!";
+    createdThing.innerText = text;
     document.body.appendChild(createdThing);
   }
 
@@ -14,6 +15,6 @@
    * Listen for messages from the background script.
    */
   browser.runtime.onMessage.addListener((message) => {
-    runStuff(message.blablah);
+    runStuff(message.blahblah);
   });
 })();
