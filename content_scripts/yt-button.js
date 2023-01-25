b7e0d1e2acac6814ed4a69bab25c5909b6bf664d
sqwirral <sqwirral@gmail.com>
2023-01-25 00:07:02 +0000

    Button loading fix and visual improvements

--- content_scripts/yt-button.js
+++ content_scripts/yt-button.js
@@ -1,29 +1,34 @@
 
 const imgURL = browser.runtime.getURL("icons/yt2clip-64.png");
 
-const element = document.querySelector(".add-to-collection-button-new");
-element.insertAdjacentHTML('afterend', 
-    '<div id="yt2clip-but"><img src="' 
-    + imgURL + '" width="16" height="16"></div>');
+document.addEventListener('yt-navigate-finish', process);
 
-const button = document.querySelector("#yt2clip-but");
-button.addEventListener('click', event => {
+function process() {
+  const element = document.querySelector(".add-to-collection-button-new");
+  console.log("element: " + element);
+  element.insertAdjacentHTML('afterend', 
+      '<div id="yt2clip-but"><img src="' 
+      + imgURL + '" width="16" height="16"></div>');
 
-  alert("You clicked da ting.");
+  const button = document.querySelector("#yt2clip-but");
+  button.addEventListener('click', event => {
 
-  const title = document.querySelector("#title h1").innerText;
-  const url = window.location.href;
-  const channel = document.querySelector("div.ytd-channel-name").innerText;
-  const subs = document.querySelector("#owner-sub-count").innerText;
-  const date = document.querySelector("#info span:nth-child(3)").innerText;
-  const views = document.querySelector("#info span").innerText;
-  const time = document.querySelector("span.ytp-time-duration").innerText;
+    alert("You clicked da ting.");
 
-  const text = title + "\n" + url + "\n" + channel + " (" + subs + ") - " 
-                + views + " - " + date + " - " + time;
-  
-  navigator.clipboard.writeText(text);
+    const title = document.querySelector("#title h1").innerText;
+    const url = window.location.href;
+    const channel = document.querySelector("div.ytd-channel-name").innerText;
+    const subs = document.querySelector("#owner-sub-count").innerText;
+    const date = document.querySelector("#info span:nth-child(3)").innerText;
+    const views = document.querySelector("#info span").innerText;
+    const time = document.querySelector("span.ytp-time-duration").innerText;
 
-  const confirmation = document.querySelector("#title h1");
-  confirmation.innerText += " [*COPIED INFO!*]";
-})
+    const text = title + "\n" + url + "\n" + channel + " (" + subs + ") - " 
+                  + views + " - " + date + " - " + time;
+    
+    navigator.clipboard.writeText(text);
+
+    const confirmation = document.querySelector("#title h1");
+    confirmation.innerText += " [*COPIED INFO!*]";
+  })
+}
